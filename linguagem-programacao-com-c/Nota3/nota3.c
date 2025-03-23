0a0e87571a9af910811b6597d233ccd446f6bcdc
agenciadigitalslz <aslfama@gmail.com>
2025-03-23 03:58:48 -0300

    att

--- linguagem-programacao-com-c/Nota3/nota3.c
+++ linguagem-programacao-com-c/Nota3/nota3.c
@@ -1,2 +1,3 @@
 // Em construção
-// Compare this snippet from estudos-ads/linguagem-programacao-com-c/Nota3/nota3_backup.c:S
+// Compare this snippet from estudos-ads/linguagem-programacao-com-c/Nota3/nota3_backup.c
+//         printf("0 - Voltar\n>>>> ");
